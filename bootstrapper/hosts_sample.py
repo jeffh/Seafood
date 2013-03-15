02ba5ef0ec48827547563a4a029b0ce2694f2673
Jeff Hui <jeff@jeffhui.net>
2013-03-14 23:39:47 -0700

    Updated hosts_sample.py

--- bootstrapper/hosts_sample.py
+++ bootstrapper/hosts_sample.py
@@ -6,9 +6,9 @@ from bootstrapper.helpers import add_host, clears
 @task
 def deploy_host():
     "Adds the deploy_host test machine to hosts"
-    add_host('jeff@192.168.1.119', 'p')
+    add_host('jeff@192.168.1.119', 'password', roles=['master'])
 
 @task
 def deploy_client():
     "Adds the deploy_client test machine to hosts"
-    add_host('jeff@192.168.1.121', 'p')
+    add_host('jeff@192.168.1.121', 'password', roles=['minion'])
