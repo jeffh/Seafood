5cae8e466a206cb888b40308798fd40303c76f3c
Jeff Hui <jeff@jeffhui.net>
2013-03-28 23:45:26 -0700

    Added code to (un)mount mac dmgs

---
+++ configurations/base/states/_modules/dmg.py
@@ -0,0 +1,19 @@
+"""
+Handles the mounting and unmounting of dmg files.
+"""
+
+def mount(name, mountpoint=None, mountroot=None):
+	args = ['hdiutil', 'attach', name]
+	if mountpoint:
+		args.extend(['-mountpoint', mountpoint])
+	if mountroot:
+		args.extend(['-mountroot', mountroot])
+
+	return __salt__['cmd.run'](' '.join(args))
+
+def unmount(name, force=False):
+	args = ['hdiutil', 'detach', name]
+	if force:
+		args.append('-f')
+
+	return __salt__['cmd.run'](' '.join(args))
